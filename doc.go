054dcc2aa35e7ab9e02a595e09e97f5e33a73f64
mauricioromagnollo <mauricioromagnollo@gmail.com>
2026-03-01 20:20:37 -0300

    chore: add lint and fix reported issues

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package flowkafka provides a high-level abstraction for working with Kafka in Go applications. It simplifies the process of producing and consuming messages, managing configurations, and handling errors, allowing developers to focus on business logic rather than the intricacies of Kafka's API.
+// The package includes features such as:
+// - Easy-to-use producer and consumer interfaces
+// - Support for both synchronous and asynchronous message processing
+// - Configurable retry mechanisms and error handling strategies
+// - Integration with popular logging libraries for better observability
+// - Compatibility with the latest versions of Kafka and Go
+package flowkafka
