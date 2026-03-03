5cab88b161d3ba6df1666aa95c7eb11d0ae61e4e
mauricioromagnollo <mauricioromagnollo@gmail.com>
2026-03-03 18:54:53 -0300

    test: add complete api compiles test

--- import_test.go
+++ import_test.go
@@ -7,6 +7,14 @@ import (
 )
 
 func TestPublicAPICompiles(_ *testing.T) {
+	// kafka
 	_ = flowkafka.KafkaConfig{}
 	_ = flowkafka.Message{}
+	_ = flowkafka.KafkaClient(nil)
+	_ = flowkafka.NewKafkaClient(flowkafka.KafkaConfig{})
+
+	// schemaregistry
+	_ = flowkafka.SchemaRegistryConfig{}
+	_ = flowkafka.SchemaRegistryClient(nil)
+	_ = flowkafka.NewSchemaRegistryClient(flowkafka.SchemaRegistryConfig{})
 }
