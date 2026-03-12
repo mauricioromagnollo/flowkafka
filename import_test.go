628597a28eb17d1f5571c220dcb45f46d890a71f
mauricioromagnollo <mauricioromagnollo@gmail.com>
2026-03-12 20:38:52 -0300

    test: add missing public api tests

--- import_test.go
+++ import_test.go
@@ -11,4 +11,18 @@ func TestPublicAPICompiles(_ *testing.T) {
 	_ = flowkafka.SchemaRegistryConfig{}
 	_ = flowkafka.SchemaRegistry(nil)
 	_ = flowkafka.NewSchemaRegistry(flowkafka.SchemaRegistryConfig{})
+
+	// Producer
+	_ = flowkafka.ProducerConfig{}
+	_ = flowkafka.Producer(nil)
+	_ = flowkafka.NewProducer(flowkafka.ProducerConfig{})
+
+	// Consumer
+	_ = flowkafka.ConsumerConfig{}
+	_ = flowkafka.Consumer(nil)
+	_ = flowkafka.NewConsumer(flowkafka.ConsumerConfig{
+		GroupID:   "any_group_id",
+		Brokers:   []string{"localhost:9092"},
+		TopicName: "any_topic_name",
+	})
 }
